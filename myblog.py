01c61f34b03c54e7a9cf0d599ea40717a842b361
zhuwenwqing <742370351@qq.com>
2018-08-09 17:34:02 +0800

    myblog.py change

--- myblog.py
+++ myblog.py
@@ -1,3 +1,7 @@
 from app import app
+from flask_login import LoginManager
 if __name__=='__main__':
+    login_manager=LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view='login'
     app.run(host='0.0.0.0',debug='True')
